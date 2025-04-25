2d9072f4dac81f4d64dccda3c97a9fa62e88246e
ArcaneDev <alive6863@gmail.com>
2025-04-25 22:51:42 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Установка тулчейна и компонентов
 rustup toolchain install nightly
 rustup component add rust-src --toolchain nightly
 
-# Установка переменных окружения ESP-IDF
 export IDF_PATH=/opt/esp-idf
 source $IDF_PATH/export.sh
-
-# Установка переменных для Rust
+espup update
 #export RUSTFLAGS="-C link-arg=-Tesp32c3/rom.ld -C link-arg=-Tesp32c3/sections.ld -C link-arg=-Tesp32c3/esp32c3.ld -C linker-plugin-lto=yes"
 export ESP_IDF_PATH=/opt/esp-idf
 export IDF_PATH=/opt/esp-idf
