1fa6831d15247e3cf18aab76408bba1f3f379d69
ArcaneDev <alive6863@gmail.com>
2025-05-20 20:30:55 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,64 @@
 #!/bin/bash
 
-#rustup toolchain install nightly
-#rustup component add rust-src --toolchain nightly
-#espup update 
+build_with_esp() {
 
-export IDF_PATH=/opt/esp-idf
-source $IDF_PATH/export.sh
-espup update
-#export RUSTFLAGS="-C link-arg=-Tesp32c3/rom.ld -C link-arg=-Tesp32c3/sections.ld -C link-arg=-Tesp32c3/esp32c3.ld -C linker-plugin-lto=yes"
-export ESP_IDF_PATH=/opt/esp-idf
-export IDF_PATH=/opt/esp-idf
-export IDF_TOOLS_PATH=~/.espressif
+    if ! command -v espup &> /dev/null; then
+        echo "Error: espup is not installed. To use this method get espup and install it. (espup install)"
+        exit 1
+    fi
 
-# Очистка и пересборка проекта
-cargo clean
-cargo update
-rustup run esp cargo build --target xtensa-esp32-espidf -Z build-std=core,compiler_builtins
-cargo +nightly build  -Z build-std=core,alloc,compiler_builtins --target xtensa-esp32-espidf.json 
-#cargo build
+    read -p "Update espup before build? (y/n): " UPDATE_ESPUP
+    if [[ $UPDATE_ESPUP == "y" ]]; then
+        espup update
+    fi
+
+    export IDF_PATH=/opt/esp-idf
+    source $IDF_PATH/export.sh
+    export ESP_IDF_PATH=/opt/esp-idf
+    export IDF_TOOLS_PATH=~/.espressif
+
+    echo "Building by espup..."
+    cargo clean
+    cargo update
+    rustup run esp cargo build --target xtensa-esp32-espidf -Z build-std=core,compiler_builtins
+}
+
+
+build_with_cargo() {
+    echo "Building by cargo LLVM target... xtensa-esp32-espidf"
+    cargo clean
+    cargo update
+    cargo +nightly build -Z build-std=core,alloc,compiler_builtins --target xtensa-esp32-espidf.json
+}
+
+build_with_cargo_elf(){
+    echo "Building by cargo LLVM target... xtensa-esp32-none-elf"
+    cargo clean
+    cargo update
+    cargo +nightly build -Z build-std=core,alloc,compiler_builtins --target xtensa-esp32-none-elf.json
+}
+
+echo "Choose build method:"
+echo "1) Build by espup (espup package must be installed. )"
+echo "2) Build by LLVM target - `xtensa-esp32-espidf.json`"
+echo "3) Build by LLCM target - `xtensa-esp32-none-elf.json`"
+echo "WARNING: Use second and third variations only if you cant get espup, build can broke or usupported target or targets conflict error.
+read -p "Enter a number (1/3): " METHOD
+
+case $METHOD in
+    1)
+        build_with_esp
+        ;;
+    2) 
+       build_with_cargo
+        ;;
+    3) 
+       build_with_cargo_elf
+        ;;
+    *)
+        echo "Uncorrect choose. Enter 1-3 number."
+        exit 1
+        ;;
+esac
+
+echo "Build closed."
