89eb64ef18cbf830acbc7ff30e85dce38eca706c
ArcaneDev <alive6863@gmail.com>
2025-05-21 22:30:17 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,9 +7,21 @@ if [[ "$1" == "-nointeractive" ]]; then
     fi
     source conf.env
     case "$build_vendor" in
-        "esp") METHOD=1 ;;
-        "cargo-espdif") METHOD=2 ;;
-        "cargo-none-elf") METHOD=3 ;;
+        "esp") METHOD=1 
+        ;;
+        
+        "cargo-espdif") METHOD=2 
+        ;;
+        
+        "cargo-none-elf") METHOD=3 
+        ;;
+        
+        "cargo-riscv") METHOD=5 
+        ;;
+        
+        "cargo-espup") METHOD=4 
+        ;;
+        
         *)
             echo "Ошибка: неверное значение build_vendor в conf.env"
             exit 1
@@ -18,11 +30,15 @@ if [[ "$1" == "-nointeractive" ]]; then
 else
 
 echo "Choose build method:"
+echo "TARGET: Xtensa32:"
 echo "1) Build by espup (espup package must be installed. )"
 echo "2) Build by LLVM target - `xtensa-esp32-espidf.json`"
-echo "3) Build by LLCM target - `xtensa-esp32-none-elf.json`"
+echo "3) Build by LLVM target - `xtensa-esp32-none-elf.json`"
 echo "WARNING: Use second and third variations only if you cant get espup, build can broke or usupported target or targets conflict error."
-read -p "Enter a number (1/3): " METHOD
+echo "TARGET: RISCV32"
+echo "4) Build by espup (espup package must be installed. )"
+echo "5) Build by default cargo target riscv32imc-none-elf (unable to install target before run build)"
+read -p "Enter a number (1/5): " METHOD
 
 fi
 
@@ -50,6 +66,35 @@ build_with_esp() {
    # --target xtensa-esp32-espidf
 }
 
+build_with_riscv_espup() {
+
+    if ! command -v espup &> /dev/null; then
+        echo "Error: espup is not installed. To use this method get espup and install it. (espup install)"
+        exit 1
+    fi
+
+    read -p "Update espup before build? (y/n): " UPDATE_ESPUP
+    if [[ $UPDATE_ESPUP == "y" ]]; then
+        espup update
+    fi
+
+    export IDF_PATH=/opt/esp-idf
+    source $IDF_PATH/export.sh
+    export ESP_IDF_PATH=/opt/esp-idf
+    export IDF_TOOLS_PATH=~/.espressif
+
+    echo "Building by espup..."
+    cargo clean
+    cargo update
+    rustup run esp cargo build -Z build-std=core,alloc,compiler_builtins --target riscv32imac-esp-espidf --release
+}
+
+build_with_riscv_cargo(){
+    echo "Building by cargo LLVM target... riscv32imc-none-elf"
+    cargo clean
+    cargo update
+    cargo +nightly build -Z build-std=core,alloc,compiler_builtins --target riscv32imc-none-elf --release
+}
 
 build_with_cargo() {
     echo "Building by cargo LLVM target... xtensa-esp32-espidf"
@@ -75,6 +120,13 @@ case $METHOD in
     3) 
        build_with_cargo_elf
         ;;
+        
+    4) build_with_riscv_espup
+        ;;
+        
+    5) build_with_riscv_cargo
+        ;;
+        
     *)
         echo "Uncorrect choose. Enter 1-3 number."
         exit 1
